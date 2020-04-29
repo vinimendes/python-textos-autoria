116dc1a4c16813c6a4de7e0a95a45cee36362714
vinimendes <vini9797@hotmail.com>
2020-04-29 15:57:03 -0300

    Definindo assinatura

---
+++ coh_piah.py
@@ -0,0 +1,18 @@
+
+
+def le_assinatura():
+    print("Bem-vindo ao detector automático de COH-PIAH.")
+    print("Informe a assinatura típica de um aluno infectado:")
+
+    wal = float(input("Entre o tamanho médio de palavra:"))
+    ttr = float(input("Entre a relação Type-Token:"))
+    hlr = float(input("Entre a Razão Hapax Legomana:"))
+    sal = float(input("Entre o tamanho médio de sentença:"))
+    sac = float(input("Entre a complexidade média da sentença:"))
+    pal = float(input("Entre o tamanho medio de frase:"))
+
+    return [wal, ttr, hlr, sal, sac, pal]
+
+
+
+le_assinatura()
